17622bc9c173fce5b2612a66262bbc2e10dcb849
Mamunul <mamunul.mazid@gmail.com>
2018-04-02 11:37:43 +0600

    Initial Commit

---
+++ FAST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FAST
+//
+//  Created by Mamunul on 4/2/18.
+//  Copyright © 2018 Mamunul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
